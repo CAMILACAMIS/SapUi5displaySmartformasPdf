d01e1491d7914930d4dd513788dea342b8728474
halilural <halilural5@gmail.com>
2018-10-28 14:31:50 +0300

    first Commit

---
+++ webapp/Component.js
@@ -0,0 +1,46 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"SapUi5displaySmartformasPdf/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("SapUi5displaySmartformasPdf.Component", {
+
+		metadata: {
+			"version": "1.0.0",
+			"rootView": {
+				viewName: "SapUi5displaySmartformasPdf.view.View1",
+				type: sap.ui.core.mvc.ViewType.XML
+			},
+			"dependencies": {
+				"libs": ["sap.ui.core", "sap.m", "sap.ui.layout"]
+			},
+			"config": {
+				"i18nBundle": "SapUi5displaySmartformasPdf.i18n.i18n",
+				"icon": "",
+				"favIcon": "",
+				"phone": "",
+				"phone@2": "",
+				"tablet": "",
+				"tablet@2": ""
+			}
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * In this method, the resource and application models are set.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			var mConfig = this.getMetadata().getConfig();
+
+			// set the i18n model
+			this.setModel(models.createResourceModel(mConfig.i18nBundle), "i18n");
+
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+		}
+	});
+});
